1b58cf4682da4f62c3d3799704c894ce1b8ed009
sgenie68 <g.soudlenkov@auckland.ac.nz>
2019-09-06 10:34:30 +1200

    Create burgers_dynamic.cmake

---
+++ tests/burgers_dynamic.cmake
@@ -0,0 +1,5 @@
+set(TEST_GIT_REPO https://github.com/OpenCMISS-Examples/burgers_dynamic)
+set(TEST_BRANCH develop)
+set(TEST_TARGETS burgers_dynamic_fortran)
+set(TEST_EXPECTED_RESULTS src/fortran/expected_results/)
+set(TEST_REL_TOLERANCE 1e-12)
