05a33e9d3d776ffd7b715e5a5b10c82e93d4f148
sgenie68 <g.soudlenkov@auckland.ac.nz>
2019-09-20 11:37:32 +1200

    Reduced tolerance

--- tests/fsi_2d_obstacle.cmake
+++ tests/fsi_2d_obstacle.cmake
@@ -3,6 +3,6 @@ set(TEST_GIT_REPO https://github.com/OpenCMISS-Examples/fsi_2d_obstacle)
 set(TEST_BRANCH develop)
 set(PYTEST_TARGETS src/python/fsi_2d_obstacle.py)
 set(PYTEST_EXPECTED_RESULTS src/python/expected_results/)
-set(PYTEST_ABS_TOLERANCE 1e-12)
-set(PYTEST_REL_TOLERANCE 1e-12)
+set(PYTEST_ABS_TOLERANCE 1e-8)
+set(PYTEST_REL_TOLERANCE 1e-8)
 
