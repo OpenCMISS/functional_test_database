07b8ed92f32c2d23d300538f086dd6614d86dac8
sgenie68 <g.soudlenkov@auckland.ac.nz>
2019-11-01 15:48:14 +1300

    Delete stokes_static.cmake
    Removed temporarily

--- tests/stokes_static.cmake
+++
@@ -1,7 +0,0 @@
-
-set(TEST_GIT_REPO https://github.com/OpenCMISS-Examples/stokes_static)
-set(TEST_BRANCH develop)
-set(TEST_TARGETS stokes_static_fortran)
-set(TEST_EXPECTED_RESULTS src/fortran/expected_results/)
-set(TEST_ABS_TOLERANCE 2e-10)
-set(TEST_REL_TOLERANCE 1e-12)
