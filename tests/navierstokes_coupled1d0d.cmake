2192b37e3fda004769484a5efb7d0d8b1fe8e3f8
sgenie68 <g.soudlenkov@auckland.ac.nz>
2019-11-01 15:47:37 +1300

    Delete navierstokes_coupled1d0d.cmake
    Removed temporarily

--- tests/navierstokes_coupled1d0d.cmake
+++
@@ -1,6 +0,0 @@
-
-set(TEST_GIT_REPO https://github.com/OpenCMISS-Examples/navierstokes_coupled1d0d)
-set(TEST_BRANCH develop)
-set(PYTEST_TARGETS src/python/coupled1D0D.py)
-set(PYTEST_EXPECTED_RESULTS src/python/expected_results/)
-set(PYTEST_REL_TOLERANCE 1e-12)
